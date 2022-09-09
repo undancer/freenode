883b5190a93f85b241fa90d82b93b4114db96401
liema <64573142+mlwrx1978@users.noreply.github.com>
2022-09-09 22:36:34 +0800

    Update speedtest.sh

--- utils/speedtest/speedtest.sh
+++ utils/speedtest/speedtest.sh
@@ -3,9 +3,9 @@ wget -O clash.gz https://github.com/MetaCubeX/Clash.Meta/releases/download/v1.13
 gunzip clash.gz
 wget -O lite.gz https://github.com/alanbobs999/LiteSpeedTest/releases/download/v0.11.2m/lite-linux-amd64-v0.11.2m.gz
 gunzip lite.gz
-wget -O clash_config.yml https://raw.githubusercontent.com/alanbobs999/TopFreeProxies/master/utils/speedtest/clash_config.yml
-wget -O proxychains.conf https://raw.githubusercontent.com/alanbobs999/TopFreeProxies/master/utils/speedtest/proxychains.conf
-wget -O lite_config.json https://raw.githubusercontent.com/alanbobs999/TopFreeProxies/master/utils/speedtest/lite_config.json
+wget -O clash_config.yml https://raw.githubusercontent.com/mlwrx1978/freenode/main/utils/speedtest/clash_config.yml
+wget -O proxychains.conf https://raw.githubusercontent.com/mlwrx1978/freenode/main/utils/speedtest/proxychains.conf
+wget -O lite_config.json https://raw.githubusercontent.com/mlwrx1978/freenode/main/utils/speedtest/lite_config.json
 #初始化 Clash
 chmod +x ./clash && ./clash &
 #安装并配置 proxychains
