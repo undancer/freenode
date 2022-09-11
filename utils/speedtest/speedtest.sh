dcc850d633841d3773ebb42a338d3c71c2ffc6c3
liema <64573142+mlwrx1978@users.noreply.github.com>
2022-09-11 23:29:36 +0800

    Update speedtest.sh

--- utils/speedtest/speedtest.sh
+++ utils/speedtest/speedtest.sh
@@ -17,4 +17,4 @@ sudo pkill -f clash
 ./clash -f clash_config.yml &
 #运行 LiteSpeedTest
 chmod +x ./lite
-sudo nohup proxychains ./lite --config ./lite_config.json --test https://raw.githubusercontent.com/mlwrx1978/freenode/master/Clash.yaml >speedtest.log 2>&1 &
+sudo nohup proxychains ./lite --config ./lite_config.json --test https://raw.githubusercontent.com/mlwrx1978/freenode/master/test.yaml >speedtest.log 2>&1 &
