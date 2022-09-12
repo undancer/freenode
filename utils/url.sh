1a3eb70290361b1744b4fac11393b8a67e2ed29f
liema <64573142+mlwrx1978@users.noreply.github.com>
2022-09-12 21:26:53 +0800

    Update url.sh

--- utils/url.sh
+++ utils/url.sh
@@ -1,5 +1,5 @@
 name=`date '+%y%m'`
 name1=`date '+%y%m%d'`
-sort update/$name/$name1.txt -o 1.txy
+sort update/$name/$name1.txt -o 1.txt
 base64 1.txt > base64
-sed -i 's/$\n//g base64 
+sed -i 's/$\n//g' base64
